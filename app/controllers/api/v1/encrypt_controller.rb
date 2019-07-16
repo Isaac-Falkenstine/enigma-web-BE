3c6c4de45a9b227bd79e37928407523c850e340a
Isaac Falkenstine <isaac@falkenstine.com>
2019-07-16 14:28:48 -0600

    Add encrypted class

--- app/controllers/api/v1/encrypt_controller.rb
+++ app/controllers/api/v1/encrypt_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::EncryptController < ApplicationController
   def index
+    message = Encrypted.new(params[:message], params[:key])
     render json: MessageSerializer.new(message)
     # render :json => {error: "Unauthorized"}, status: 403
   end
---
+++ app/models/encrypted.rb
@@ -0,0 +1,6 @@
+class Encrypted
+  def initialize(message, key)
+    @message     = message
+    @key   = key
+  end
+end
