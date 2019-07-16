2bcd06f9229fe9de64df2b8ce9790e3b2a88b9bc
Isaac Falkenstine <isaac@falkenstine.com>
2019-07-16 14:21:15 -0600

    Add message serializer

--- app/controllers/api/v1/encrypt_controller.rb
+++ app/controllers/api/v1/encrypt_controller.rb
@@ -1,3 +1,6 @@
 class Api::V1::EncryptController < ApplicationController
-  
+  def index
+    render json: MessageSerializer.new(message)
+    # render :json => {error: "Unauthorized"}, status: 403
+  end
 end
---
+++ app/serializers/message_serializer.rb
@@ -0,0 +1,6 @@
+class MessageSerializer
+  include FastJsonapi::ObjectSerializer
+
+  # attributes :sender, :receiver, :status, :meeting_date, :meeting_location, :created_at
+
+end
